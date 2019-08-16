c73b1839177b4f699fe71796464cefbc40c21c24
kubersharma001 <kubersharma001@gmail.com>
2019-08-16 21:12:42 +0530

    axon7: Go permissive and disable device sepolicy
    * define libinit_msm8996 in BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,6 +69,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
@@ -217,9 +218,13 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 VENDOR_SECURITY_PATCH := 2018-07-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+
+# Vendor init
+TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_msm8996
+TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8996
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
