5eb9cd40f814b6e99a92904719f2d9635f2738ae
baddar90 <mnbaddar1990@gmail.com>
2019-11-20 17:36:36 +0000

    axon7: Bring sensors back to life
    The old blobs no longer seem to work, causing the sensor service
    to indefinitely try to register. Just kang some cool blobs and align
    permission sets and IPC sec configs with CAF.

--- config.fs
+++ config.fs
@@ -13,6 +13,12 @@ value:2904
 [AID_VENDOR_ADPL_ODL]
 value:2905
 
+[vendor/bin/sensors.qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [vendor/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
