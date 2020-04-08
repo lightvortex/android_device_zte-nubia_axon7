1aa4df8030326b782f60d2201745be94ee9255b2
Bruno Martins <bgcngm@gmail.com>
2020-04-08 22:52:46 +0100

    Axon7: Override SDK API level for mm-qcamera-daemon
    Replace file pinning applied in commit fee6d01 by setting the lower SDK version
    that exempts mm-qcamera-daemon from the tight restrictions on mutex calls.
    
    This allows us to update to the latest blobs without needing to manually
    re-patch the offending camera lib.
    
    Change-Id: I1d95a4019c298497dce504894d0db705e541a92d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -126,9 +126,13 @@ QCOM_BT_USE_BTNV := true
 QCOM_BT_USE_SMD_TTY := true
 
 # Camera
+BOARD_QTI_CAMERA_32BIT_ONLY := true
 TARGET_USES_MEDIA_EXTENSIONS := true
 TARGET_USES_QTI_CAMERA_DEVICE := true
 USE_DEVICE_SPECIFIC_CAMERA := true
+
+TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
+    /vendor/bin/mm-qcamera-daemon=26
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
