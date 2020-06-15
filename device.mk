9684b5bc4b1ead50eba05552d42ac7273f8bfacf
lightvortex <rishishukla242@gmail.com>
2020-06-15 09:53:03 +0530

    axon7:switch to prebuilt biometrics hal

--- device.mk
+++ device.mk
@@ -295,7 +295,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1 \
+    libunwind
 
 # Media
 PRODUCT_COPY_FILES += \
